709a7c89b1728ff09852aa28ec62b48428cf8a08
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-05-06 21:15:14 -0300

    Create scripts.ts
    Signed-off-by: Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>

---
+++ utils/scripts.ts
@@ -0,0 +1,14 @@
+export const scripts = [
+  {
+    slug: 'hub-do-dk',
+    name: 'Hub do DK',
+    description: 'O hub mais insano da quebrada',
+    rawScript: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/seuuser/seurepo/main/hub.lua"))()'
+  },
+  {
+    slug: 'esp-hack',
+    name: 'ESP Hack',
+    description: 'Mostra inimigos com estilo',
+    rawScript: 'loadstring(game:HttpGet("https://raw.githubusercontent.com/seuuser/seurepo/main/esp.lua"))()'
+  }
+]
