24be87ba7b97eabb5bd6b00a2688d3f773360088
Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>
2025-05-06 21:24:35 -0300

    Update index.tsx
    Signed-off-by: Dkzin.rlk <79470887+SrDark222@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
 import Link from 'next/link'
-import { scripts } from '@/utils/scripts'
+
+const scripts = [
+  { slug: 'hub-do-dk', name: 'Hub do DK' },
+  { slug: 'esp-hack', name: 'ESP Hack' }
+]
 
 export default function Home() {
   return (
     <div style={{ padding: '2rem' }}>
       <h1>Scripts do DK</h1>
       <ul>
-        {scripts.map((script) => (
-          <li key={script.slug}>
-            <Link href={`/${script.slug}`}>
-              {script.name} - {script.description}
-            </Link>
+        {scripts.map(s => (
+          <li key={s.slug}>
+            <Link href={`/${s.slug}`}>{s.name}</Link>
           </li>
         ))}
       </ul>
